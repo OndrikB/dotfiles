a4612ac3681d3e73ec3c5e45ac9d27a1a3156a25
OndrikB <ondricek.baco@gmail.com>
2023-04-21 20:39:39 +0200

    slight changes to nvim setup
    might change colors again
    LSP changes coming SoonTM

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,23 +15,22 @@ vim.g.mapleader = " "
 
 require("lazy").setup("plugins")
 
-vim.cmd [[set relativenumber]]
-vim.cmd [[set termguicolors]]
+vim.cmd.colorscheme("falcon")
+vim.cmd.set("relativenumber")
+vim.cmd.set("termguicolors")
+-- vim.cmd [[set relativenumber]]
+-- vim.cmd [[set termguicolors]]
 
---[[
-vim.g.moonflyTerminalColors = false
+
 
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+-- TODO: this requires ripgrep! Install it!
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 
-vim.cmd [[colorscheme moonfly]]
 
---]]
-
-vim.cmd [[colorscheme pablo]]
 
 local cmp = require'cmp'
 
@@ -62,7 +61,7 @@ cmp.setup({
 })
 
 
-require('lualine').setup()
+require("lualine").setup()
 require("mason").setup()
 require("mason-lspconfig").setup()
 
---
+++ .config/nvim/lua/plugins/falcon_colors.lua
@@ -0,0 +1,5 @@
+return {
+	{
+		"fenetikm/falcon",
+	},
+}
