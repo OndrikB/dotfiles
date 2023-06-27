053bb14f3d9eec6369a1a4300396a29725aa258d
OndrikB <ondricek.baco@gmail.com>
2023-06-27 20:34:15 +0200

    background + nvim
    - background changes now excludes the current background
     - backgrounds placed in separate folder
     - some neovim changes

--- .config/nvim/lua/ondrik/remap.lua
+++ .config/nvim/lua/ondrik/remap.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, {desc = 'Directory listing (NetRW)'})
 
 
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
@@ -7,3 +7,9 @@ vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { de
 -- TODO: this requires ripgrep! Install it!
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+
+
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, { desc = '[E]rrors'})
+
+
+
--- .config/nvim/lua/plugins/tree-sitter.lua
+++ .config/nvim/lua/plugins/tree-sitter.lua
@@ -3,6 +3,7 @@ local M = {
     build = function()
         require("nvim-treesitter.install").update({ with_sync = true })
     end,
+    dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' }
 }
 
 return { M }
